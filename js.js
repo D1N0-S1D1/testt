e626f57c35757d41f4f771b7fb5c56a2915e1686
D1N0-S1D1 <dmytro__shevchuk@ukr.net>
2024-11-30 18:41:30 +0200

    123123

--- js.js
+++ js.js
@@ -1,41 +1,24 @@
-url = 'https://jsonplaceholder.typicode.com/posts'
-let sector = document.getElementById('sector')
+const grid = document.getElementById('content')
 
-fetch(url)
+fetch('https://my-json-server.typicode.com/D1N0-S1D1/testt/db')
     .then(async function(response) {
-        let data = await response.json();
-        console.log(data);
+        let data = await response.json()
+        console.log(data)
 
-        let filtered = data.filter(function(filtData){
-            return filtData.userId < 10
-        })
-
-        let sortData = filtered.sort(function(first, second){
-            if(first.title.length < second.title.length) return -1;
-            if(first.title.length > second.title.length) return 1;
-            return 0
-            
-        })
-
-        sortData.forEach(element => {
-        addsector(element)
-        })
+        data.products.forEach(element => {
+            addcard(element)
+            })
+    })
 
 
-    });
-
-    git_url = 
-
-
-
-    function addsector(sec){
-        sector.innerHTML += `
-        <div class = 'card'>
-        <h1>TITLE: ${sec.title}</h1>
-        <h2>USER ID: ${sec.userId}</h2>
-        <h3>ID: ${sec.id}</h3>
-        <p>Body: ${sec.body}</p>
+function addcard(eve){
+    grid.innerHTML += `
+        <div class="card">
+            <h1>Name: ${eve.name}</h1>
+            <h3>ID: ${eve.id}</h3>
+            <p>${eve.description}</p>
+            <img src="${eve.photo_url}" alt="">
         </div>
-        <hr>
-        `    
-    }
+    `
+    console.log(eve.photo_url)
+}    
